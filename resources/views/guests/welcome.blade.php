0204305db29bcfad5ce9058af5adcfb8e504bcdf
Jordan <jcsisphat@Jordans-MacBook-Pro.local>
2015-04-23 09:20:07 +1000

    Ready for Test Push to Production

--- resources/views/guests/welcome.blade.php
+++ resources/views/guests/welcome.blade.php
@@ -4,12 +4,13 @@
 	<div class="row">
 		
 		<div class="small-12 large-12 columns welcome-banner"><img src="img/Logo.jpg" width="300px" height="100px"></div>
-		<div class="small-12 large-12 columns welcome-headline">Welcome to the PSS Staff Portal!</div>
-		<div class="small-12 large-12 columns welcome-buttons" >
+		<div class="small-12 large-12 columns welcome-headline">Welcome to the Future PSS Staff Portal!</div>
+		{{-- <div class="small-12 large-12 columns welcome-buttons" >
 			<a class="button success radius" href="{{ route('login') }}">Log In</a>
 			&nbsp or &nbsp
 			<a class="button radius" href="{{ route('create') }}">Create Account</a>
-		</div>
+		</div> --}}
+		<div class="center-text small-12 large-12 columns">Beta Coming Soon!</div>
 		
 
 	</div>
